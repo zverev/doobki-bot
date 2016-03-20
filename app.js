ec2e1d91252d8a2a9d9570080a3795082d2c3dc3
zverev <alexander.zverev@gmail.com>
2016-03-20 17:25:31 +0300

    minimal telegram bot

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var env = process.env;
+var TelegramBot = require('node-telegram-bot-api');
+
+var token = env.PROPANEBOT_TOKEN;
+
+if (!token) {
+    console.error('set PROPANEBOT_TOKEN');
+    return;
+}
 
 var server = http.createServer(function(req, res) {
     var url = req.url
@@ -18,4 +26,14 @@ var server = http.createServer(function(req, res) {
 
 server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function() {
     console.log(`Application worker ${process.pid} started...`);
+});
+
+// Setup polling way
+var bot = new TelegramBot(token, {
+    polling: true
+});
+
+// Any kind of message
+bot.on('message', function(msg) {
+    bot.sendMessage(msg.chat.id, 'хуй');
 });
